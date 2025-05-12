91a801d34f83c5acd28b9e3c63250a735a6678e7
Smilingvk <valeriaguzmanavalos@gmail.com>
2025-05-12 16:51:51 -0500

    exercise4 solved

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int>numbers = new List<int>();
+        int sum = 0;
+        int cnt = 0;
+        int largest = 0;
+        while(true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                break;
+            }else{
+                numbers.Add(number);
+                sum+=number;
+                cnt++;
+                if(number>largest)largest = number;
+            }
+
+        }
+        int smallest = largest;
+        foreach (int num in numbers)
+        {
+            if(num<=smallest && num>0)smallest=num;
+        }
+        double average  = (double)sum/cnt;
+        numbers.Sort();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"Thea average is: {average}");
+        Console.WriteLine($"The largest unmber is: {largest}");
+        Console.WriteLine($"The smallest positive bumber is: {smallest}");
+        Console.WriteLine($"The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine($"{num}");
+        }
     }
 }
