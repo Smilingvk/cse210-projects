47f5811e8d2c9113b22a50ef020109dfc9c7639d
Smilingvk <valeriaguzmanavalos@gmail.com>
2025-05-26 19:40:40 -0500

    done

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,80 @@
 using System;
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
 
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom;
+    }
+
+    public int GetNumerator()
+    {
+        return numerator;
+    }
+
+    public void SetNumerator(int value)
+    {
+        numerator = value;
+    }
+
+    public int GetDenominator()
+    {
+        return denominator;
+    }
+
+    public void SetDenominator(int value)
+    {
+        denominator = value;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();          
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);        
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);    
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);      
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        f4.SetNumerator(2);
+        f4.SetDenominator(5);
+        Console.WriteLine(f4.GetFractionString());  
+        Console.WriteLine(f4.GetDecimalValue());   
     }
 }
